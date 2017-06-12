4ac450ace7bdaeae34a34949ece675d1a45a1fbe
yu3mars <yu3mars@users.noreply.github.com>
2017-06-13 00:57:00 +0900

    post bye when closing

--- bot.rb
+++ bot.rb
@@ -10,20 +10,23 @@ Slack.configure do |config|
 		  fail 'Missing ENV[SLACK_API_TOKEN]!' unless config.token
 end
 
-client = Slack::Web::Client.new
+clientWeb = Slack::Web::Client.new
 
-client.auth_test
+clientWeb.auth_test
 
-#client.chat_postMessage(channel: '#yu3-dev-memo', text: 'Hello World', as_user: true)
-#client.chat_postMessage(channel: '#yu3-dev-memo', text: 'Hello World', as_user: false)
-#client.chat_postMessage(channel: '#yu3-dev-memo', text: 'Hello World', username: "yubot", as_user: false, icon_emoji: ":3:")
+#clientWeb.chat_postMessage(channel: '#yu3-dev-memo', text: 'Hello World', as_user: true)
+#clientWeb.chat_postMessage(channel: '#yu3-dev-memo', text: 'Hello World', as_user: false)
+#clientWeb.chat_postMessage(channel: '#yu3-dev-memo', text: 'Hello World', username: "yubot", as_user: false, icon_emoji: ":3:")
 
 client = Slack::RealTime::Client.new
 
+# 接続時
 client.on :hello do
   puts "Successfully connected, welcome '#{client.self.name}' to the '#{client.team.name}' team at https://#{client.team.domain}.slack.com."
+  clientWeb.chat_postMessage(channel: '#yu3-dev-memo', text: 'Hello World', username: "yubot", as_user: false, icon_emoji: ":3:")
 end
 
+# 誰かが発言したとき
 client.on :message do |data|
   case data.text
   when 'bot hi' then
@@ -33,8 +36,10 @@ client.on :message do |data|
   end
 end
 
+# 接続解除時
 client.on :close do |_data|
   puts "Client is about to disconnect"
+  clientWeb.chat_postMessage(channel: '#yu3-dev-memo', text: 'Bye', username: "yubot", as_user: false, icon_emoji: ":3:")  
 end
 
 client.on :closed do |_data|
